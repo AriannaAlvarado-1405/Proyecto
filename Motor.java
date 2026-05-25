0504422a59602ff188d18a772e73442f3dae9aee
AriannaAlvarado-1405 <arianna.alvarado@udla.edu.ec>
2026-05-25 09:48:32 -0500

    Add voltajeNominal to Motor class
    Added voltajeNominal attribute and updated constructor.

--- Motor.java
+++ Motor.java
@@ -6,13 +6,15 @@ public class Motor {
     private String tipoMotor;
     private double potenciaNominal;
     private double corrienteNominal;
+    private double voltajeNominal;
     private String ubicacion;
 
-    public Motor(String codigo, String tipoMotor, double potenciaNominal, double corrienteNominal, String ubicacion) {
+    public Motor(String codigo, String tipoMotor, double potenciaNominal, double corrienteNominal, double voltajeNominal, String ubicacion) {
         this.codigo = codigo;
         this.tipoMotor = tipoMotor;
         this.potenciaNominal = potenciaNominal;
         this.corrienteNominal = corrienteNominal;
+        this.voltajeNominal= voltajeNominal;
         this.ubicacion = ubicacion;
     }
 
@@ -38,18 +40,22 @@ public class Motor {
     }
 
     public void setPotenciaNominal(double potenciaNominal) {
+        
         this.potenciaNominal = potenciaNominal;
     }
 
     public double getCorrienteNominal() {
+        
         return corrienteNominal;
     }
 
     public void setCorrienteNominal(double corrienteNominal) {
+        
         this.corrienteNominal = corrienteNominal;
     }
 
     public String getUbicacion() {
+        
         return ubicacion;
     }
 
@@ -57,13 +63,23 @@ public class Motor {
         this.ubicacion = ubicacion;
     }
 
+    public double getVoltajeNominal() {
+        return voltajeNominal;
+    }
+
+    public void setVoltajeNominal(double voltajeNominal) {
+        this.voltajeNominal = voltajeNominal;
+    }
+
     public String mostrarInfo() {
         return "Codigo: " + codigo +
                 ", Tipo: " + tipoMotor +
                 ", Potencia: " + potenciaNominal +
                 ", Corriente: " + corrienteNominal +
+                " ,Voltaje: "+voltajeNominal+
                 ", Ubicacion: " + ubicacion;
     }
 }
+
 
 
